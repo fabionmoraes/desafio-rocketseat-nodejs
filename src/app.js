4864ac6efe099c9f35eac4fb65b514fed7f90aec
Fabio Moraes <fabio@elaboredigital.com.br>
2020-07-21 11:37:07 -0300

    update repositorie

---
+++ src/app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+
+const { uuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { url, techs } = request.body;
+
+  const repositorie = { id: uuid(), url, techs, like: 0 }
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const { url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(item => item.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' });
+  }
+
+  const { like } = repositories.find(item => item.id === id);
+
+  const repositorie = { id, url, techs};
+
+  repositories[repositorieIndex] = { ...repositorie, like };
+  
+  return response.json(repositorie);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(item => item.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' });
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(item => item.id === id);
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({ error: 'Repositorie not found' });
+  }
+
+  const { url, like, techs } = repositories.find(item => item.id === id);
+
+  const count = like + 1;
+
+  const repositorieLike = { id, url, techs, like: count };
+
+  repositories[repositorieIndex] = repositorieLike;
+  
+  return response.json(count);
+});
+
+module.exports = app;
